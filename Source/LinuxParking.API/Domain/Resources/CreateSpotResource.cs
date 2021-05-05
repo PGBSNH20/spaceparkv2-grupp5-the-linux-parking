a312befb87fee451c1696d18bf28675d2b039c9b
Adam Brodin <worecraft@gmail.com>
2021-05-05 23:02:31 +0200

    Changes according to pull-request feedback

--- Source/LinuxParking.API/Domain/Resources/CreateSpotResource.cs
+++ Source/LinuxParking.API/Domain/Resources/CreateSpotResource.cs
@@ -5,8 +5,10 @@ namespace LinuxParking.API.Domain.Resources
     public class CreateSpotResource
     {
         [Required]
+        [MinLength(1)]
         public int Size { get; set; }
         [Required]
+        [MinLength(1)]
         public decimal Price { get; set; }
     }
 }
