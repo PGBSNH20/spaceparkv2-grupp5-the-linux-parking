2822b3a75bd2d01af3747b8ce9e4bd0c646bd7b7
Adam Brodin <worecraft@gmail.com>
2021-05-07 13:41:22 +0200

    IParkingService

--- Source/LinuxParking.API/Domain/Interfaces/Services/IParkingService.cs
+++ Source/LinuxParking.API/Domain/Interfaces/Services/IParkingService.cs
@@ -1,7 +1,15 @@
+using System.Threading.Tasks;
+using LinuxParking.API.Domain.Models;
+using LinuxParking.API.Domain.Response;
+
 namespace LinuxParking.API.Domain.Interfaces.Services
 {
     public interface IParkingService
     {
-
+        Task<ParkingResponse> ListAsync(int stationId);
+        Task<ParkingResponse> FindByIdAsync(int stationId, int parkingId);
+        Task<ParkingResponse> SaveAsync(ParkingStatus parking);
+        Task<ParkingResponse> UpdateAsync(int stationId, int parkingId, ParkingStatus parking);
+        Task<ParkingResponse> DeleteAsync(int stationId, int parkingId);
     }
 }
