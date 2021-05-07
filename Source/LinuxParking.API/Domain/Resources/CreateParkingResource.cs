45c06db4f9f433bc41fd3253a17b08b18976612e
Adam Brodin <worecraft@gmail.com>
2021-05-07 13:59:56 +0200

    ParkingService

--- Source/LinuxParking.API/Domain/Resources/CreateParkingResource.cs
+++ Source/LinuxParking.API/Domain/Resources/CreateParkingResource.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LinuxParking.API.Domain.Resources
 {
     public class CreateParkingResource
     {
-
+        [Required]
+        public int CustomerId;
+        [Required]
+        public int ParkingId;
     }
 }
--- Source/LinuxParking.API/Domain/Resources/ParkingResource.cs
+++ Source/LinuxParking.API/Domain/Resources/ParkingResource.cs
@@ -2,6 +2,7 @@ namespace LinuxParking.API.Domain.Resources
 {
     public class ParkingResource
     {
-        
+        public int CustomerId { get; set; }
+        public int ParkingId { get; set; }
     }
 }
