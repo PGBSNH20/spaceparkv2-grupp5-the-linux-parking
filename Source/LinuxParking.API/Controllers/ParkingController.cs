18b727f3f42d98056ecc5c0113cea61cb9f44a50
Adam Brodin <worecraft@gmail.com>
2021-05-07 13:46:43 +0200

    ParkingResponse & CreateAsync

--- Source/LinuxParking.API/Controllers/ParkingController.cs
+++ Source/LinuxParking.API/Controllers/ParkingController.cs
@@ -1,7 +1,44 @@
+using System.Threading.Tasks;
+using AutoMapper;
+using LinuxParking.API.Domain.Interfaces.Services;
+using LinuxParking.API.Domain.Models;
+using LinuxParking.API.Domain.Resources;
+using LinuxParking.API.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
 namespace LinuxParking.API.Controllers
 {
-    public class ParkingController
+    [Route("/api/parking/{stationId}/[controller]")]
+    public class ParkingController : Controller
     {
-        
+        private readonly IParkingService _parkingService;
+        private readonly IMapper _mapper;
+
+        public ParkingController(IParkingService parkingService, IMapper mapper)
+        {
+            _parkingService = parkingService;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync([FromRoute] int stationId, [FromBody] CreateParkingResource resource)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState.GetErrorMessages());
+            }
+
+            var parking = _mapper.Map<CreateParkingResource, ParkingStatus>(resource);
+            parking.StationId = stationId;
+            var res = await _parkingService.SaveAsync(parking);
+
+            if (!res.Success)
+            {
+                return BadRequest(res.Message);
+            }
+
+            var parkingResource = _mapper.Map<ParkingStatus, ParkingResource>(res.ParkingStatus);
+            return Created("", parkingResource);
+        }
     }
 }
--- Source/LinuxParking.API/Domain/Response/ParkingResponse.cs
+++ Source/LinuxParking.API/Domain/Response/ParkingResponse.cs
@@ -1,7 +1,28 @@
+using System.Collections.Generic;
+using LinuxParking.API.Domain.Models;
+
 namespace LinuxParking.API.Domain.Response
 {
-    public class ParkingResponse
+    public class ParkingResponse : BaseResponse
     {
-        
+        public ParkingStatus ParkingStatus { get; }
+        public IEnumerable<ParkingStatus> ParkingStatuses { get; }
+        private ParkingResponse(bool success, string message, ParkingStatus parkingStatus) : base(success, message)
+        {
+            ParkingStatus = parkingStatus;
+        }
+
+        private ParkingResponse(bool success, IEnumerable<ParkingStatus> parkingStatuses) : base(success)
+        {
+            ParkingStatuses = parkingStatuses;
+            Message = null;
+        }
+
+        // When creation succeded.
+        public ParkingResponse(ParkingStatus parkingStatus) : this(true, string.Empty, parkingStatus) { }
+        public ParkingResponse(IEnumerable<ParkingStatus> parkingStatuses) : this(true, parkingStatuses) { }
+
+        // When creation failed.
+        public ParkingResponse(string msg) : this(false, msg, null) { }
     }
 }
